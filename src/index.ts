e34fa3a5891147dee00169bc47130d40439a62eb
maxhha <devmaxhha@gmail.com>
2021-04-04 22:03:33 +0300

    add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world!")
+#!/usr/bin/env node
+
+import yargs from "yargs"
+import { hideBin } from "yargs/helpers"
+
+yargs(hideBin(process.argv))
+  .command(
+    "curl <url>",
+    "fetch the contents of the URL",
+    () => {},
+    (argv) => {
+      console.info(argv)
+    }
+  )
+  .demandCommand(1).argv
