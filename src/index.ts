2f1795042d0fa0b639f3ff73b3af0c4e6a488420
maxhha <devmaxhha@gmail.com>
2021-04-05 22:24:44 +0300

    add option config

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
 
 yargs(hideBin(process.argv))
-  .command(
-    "curl <url>",
-    "fetch the contents of the URL",
+  .command<{ packages: Array<string> }>(
+    ["install <packages...>", "i"],
+    "add packages to your dependencies",
     () => {},
     (argv) => {
       console.info(argv)
     }
   )
+  .option("config", {
+    type: "string",
+    description: "path to file with configuration",
+    default: "package.json",
+  })
   .demandCommand(1).argv
