bd5922f7a9f4a012a53ff686b818c3fd218d6898
maxhha <devmaxhha@gmail.com>
2021-04-05 22:55:22 +0300

    add simple deps adding

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 #!/usr/bin/env node
 
-import yargs from "yargs"
+import { readFileSync, writeFileSync } from "fs"
+import yargs, { config } from "yargs"
 import { hideBin } from "yargs/helpers"
 
 yargs(hideBin(process.argv))
-  .command<{ packages: Array<string> }>(
+  .command<{ packages: string[]; config: string }>(
     ["install <packages...>", "i"],
     "add packages to your dependencies",
     () => {},
     (argv) => {
-      console.info(argv)
+      console.debug(`Read data from config "${argv.config}"`)
+      const data = readFileSync(argv.config, "utf8")
+
+      console.debug("Parse json")
+      const parsed = JSON.parse(data)
+
+      console.debug("Add new dependencies")
+      if (!parsed.dependencies) {
+        parsed.dependencies = {}
+      }
+
+      for (let dep of argv.packages)
+        parsed.dependencies[dep] = "**here should be version**"
+
+      console.debug("Save file")
+      writeFileSync(argv.config, JSON.stringify(parsed))
     }
   )
   .option("config", {
